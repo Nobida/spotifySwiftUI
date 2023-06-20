11c1ce2af265b8e44eefff8e8c68b79035760c80
Nobida <xiyuwangkaixi@yahoo.co.jp>
2023-06-20 22:35:24 +0900

    增加空白页

---
+++ Spotify/View/Search/EmptyStateView.swift
@@ -0,0 +1,33 @@
+//
+//  EmptyStateView.swift
+//  Spotify
+//
+//  Created by Joseph Wang on 2023/6/19.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+
+struct EmptyStateView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Image(systemName: "music.note")
+                .font(.system(size: 85))
+                .padding(.bottom)
+                Text("Start your search for music")
+                .font(.title)
+            Spacer()
+        }
+        .padding()
+        .foregroundColor(Color(.systemIndigo))
+    }
+}
+
+struct EmptyStateView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmptyStateView()
+    }
+}
